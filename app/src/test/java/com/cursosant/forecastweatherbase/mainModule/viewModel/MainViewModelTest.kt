57277f9bda760901b061be333ca85de538fe3be2
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-07 15:53:23 +0200

    refactor:update to DispatchersRule
    -Commented lines from "default" implementation, this one throws a Dispatcher's error, solved by applying LiveDataTestUtil.kt class
    - Section 7/3-84

--- app/src/test/java/com/cursosant/forecastweatherbase/mainModule/viewModel/MainViewModelTest.kt
+++ app/src/test/java/com/cursosant/forecastweatherbase/mainModule/viewModel/MainViewModelTest.kt
@@ -1,11 +1,9 @@
 package com.cursosant.forecastweatherbase.mainModule.viewModel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
 import com.cursosant.forecastweatherbase.BuildConfig
 import com.cursosant.forecastweatherbase.common.dataAccess.WeatherForecastService
-import com.cursosant.forecastweatherbase.entities.WeatherForecastEntity
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
@@ -15,7 +13,6 @@ import org.junit.Rule
 import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
 
 class MainViewModelTest {
     private lateinit var mainViewModel: MainViewModel
@@ -23,12 +20,15 @@ class MainViewModelTest {
 
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
+
+    /*    @get:Rule
+        val mainCoroutinesRule = MainCoroutineRule() */
     @get:Rule
-    val mainCoroutinesRule = MainCoroutineRule()
+    val mainDispatchersRule = MainDispatcherRule()
 
     companion object {
-        private lateinit var retrofit : Retrofit
-        private lateinit var appId : String
+        private lateinit var retrofit: Retrofit
+        private lateinit var appId: String
 
         @BeforeClass
         @JvmStatic
@@ -50,51 +50,63 @@ class MainViewModelTest {
     }
 
     @Test
-    fun checkCurrentWeatherisNotNullTest() {
-        runBlocking {
-            val result = service.getWeatherForecastByCoordinates(19.4342, -99.1962, appId,
-                "metric", "en")
+    fun checkCurrentWeatherisNotNullTest() = runTest {
+        val result = service.getWeatherForecastByCoordinates(
+            19.4342, -99.1962, appId,
+            "metric", "en"
+        )
 
-            assertThat(result.current, `is`(notNullValue()))
-        }
+        assertThat(result.current, `is`(notNullValue()))
     }
 
     @Test
-    fun checkCurrentTimezoneReturnsMexico() {
-        runBlocking {
-            val result = service.getWeatherForecastByCoordinates(19.4342, -99.1962, appId,
-                "metric", "en")
+    fun checkCurrentTimezoneReturnsMexico() = runTest {
+        val result = service.getWeatherForecastByCoordinates(
+            19.4342, -99.1962, appId,
+            "metric", "en"
+        )
 
-            assertThat(result.timezone, `is`("America/Mexico_City"))
+        assertThat(result.timezone, `is`("America/Mexico_City"))
+    }
+
+    @Test
+    fun checkErrorResponseWithOnlyCoordinatesTest() = runTest {
+        try {
+            val result = service.getWeatherForecastByCoordinates(
+                19.4342, -99.1962, "",
+                "", ""
+            )
+        } catch (e: Exception) {
+            assertThat(e.localizedMessage, `is`("HTTP 401 Unauthorized"))
         }
     }
-    @Test
-    fun checkErrorResponseWithOnlyCoordinatesTest() {
-        runBlocking {
-            try {
-                val result = service.getWeatherForecastByCoordinates(19.4342, -99.1962, "",
-                    "", "")
-            } catch(e: Exception) {
-                assertThat(e.localizedMessage, `is`("HTTP 401 Unauthorized"))
+
+    //TODO this test implementation belongs to deprecated Rule 'mainCoroutinesRule' from above
+    /*    @Test
+        fun checkHourlySizeTest() {
+            runBlocking {
+               // val observer = Observer<WeatherForecastEntity> {}
+
+                //try {
+                   // mainViewModel.getResult().observeForever(observer)
+                    mainViewModel.getWeatherAndForecast(19.4342, -99.1962, appId,
+                        "metric", "en")
+
+                    val result = mainViewModel.getResult().getOrAwaitValue()
+                    assertThat(result.hourly.size, `is`(48))
+                //} finally {
+                   // mainViewModel.getResult().removeObserver(observer)
+                //}
             }
-        }
-    }
+        }  */
 
     @Test
-    fun checkHourlySizeTest() {
-        runBlocking {
-           // val observer = Observer<WeatherForecastEntity> {}
+    fun checkHourlySizeTest() = runTest {
+        mainViewModel.getWeatherAndForecast(
+            19.4342, -99.1962, appId,
+            "metric", "en"
+        )
 
-            //try {
-               // mainViewModel.getResult().observeForever(observer)
-                mainViewModel.getWeatherAndForecast(19.4342, -99.1962, appId,
-                    "metric", "en")
-
-                val result = mainViewModel.getResult().getOrAwaitValue()
-                assertThat(result.hourly.size, `is`(48))
-            //} finally {
-               // mainViewModel.getResult().removeObserver(observer)
-            //}
-        }
+        val result = mainViewModel.getResult().getOrAwaitValue()
+        assertThat(result.hourly.size, `is`(48))
     }
-}
