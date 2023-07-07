f93d8c9a9f0dbee085a4e624b8c1d95264ce087a
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-07 15:39:23 +0200

    test: create checkHourlySizeTest test
    -Commented lines from "default" implementation, this one throws a Dispatcher's error, solved by applying LiveDataTestUtil.kt class
    - Section 7/82

--- app/src/test/java/com/cursosant/forecastweatherbase/mainModule/viewModel/MainViewModelTest.kt
+++ app/src/test/java/com/cursosant/forecastweatherbase/mainModule/viewModel/MainViewModelTest.kt
@@ -1,27 +1,41 @@
 package com.cursosant.forecastweatherbase.mainModule.viewModel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.cursosant.forecastweatherbase.BuildConfig
 import com.cursosant.forecastweatherbase.common.dataAccess.WeatherForecastService
+import com.cursosant.forecastweatherbase.entities.WeatherForecastEntity
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.BeforeClass
+import org.junit.Rule
 import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 class MainViewModelTest {
     private lateinit var mainViewModel: MainViewModel
     private lateinit var service: WeatherForecastService
 
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+    @get:Rule
+    val mainCoroutinesRule = MainCoroutineRule()
+
     companion object {
         private lateinit var retrofit : Retrofit
+        private lateinit var appId : String
 
         @BeforeClass
         @JvmStatic
         fun setUpCommon() {
+
+            appId = BuildConfig.OPEN_WEATHER_KEY
+
             retrofit = Retrofit.Builder()
                 .baseUrl("https://api.openweathermap.org/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -38,7 +52,6 @@ class MainViewModelTest {
     @Test
     fun checkCurrentWeatherisNotNullTest() {
         runBlocking {
-            val appId = BuildConfig.OPEN_WEATHER_KEY
             val result = service.getWeatherForecastByCoordinates(19.4342, -99.1962, appId,
                 "metric", "en")
 
@@ -49,7 +62,6 @@ class MainViewModelTest {
     @Test
     fun checkCurrentTimezoneReturnsMexico() {
         runBlocking {
-            val appId = BuildConfig.OPEN_WEATHER_KEY
             val result = service.getWeatherForecastByCoordinates(19.4342, -99.1962, appId,
                 "metric", "en")
 
@@ -65,6 +77,24 @@ class MainViewModelTest {
             } catch(e: Exception) {
                 assertThat(e.localizedMessage, `is`("HTTP 401 Unauthorized"))
             }
+        }
+    }
+
+    @Test
+    fun checkHourlySizeTest() {
+        runBlocking {
+           // val observer = Observer<WeatherForecastEntity> {}
+
+            //try {
+               // mainViewModel.getResult().observeForever(observer)
+                mainViewModel.getWeatherAndForecast(19.4342, -99.1962, appId,
+                    "metric", "en")
+
+                val result = mainViewModel.getResult().getOrAwaitValue()
+                assertThat(result.hourly.size, `is`(48))
+            //} finally {
+               // mainViewModel.getResult().removeObserver(observer)
+            //}
         }
     }
 }
